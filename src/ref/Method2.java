6cf6a74bcd7e51da0c499d6f26ded36ade80d82d
sprtho <sprtho@naver.com>
2026-03-08 22:29:13 +0900

    2026-03-08 Null, ex

--- src/ref/Method2.java
+++ src/ref/Method2.java
@@ -2,6 +2,8 @@ package ref;
 
 public class Method2 {
 
+    // 대원칙 : 자바는 항상 변수의 값을 복사해서 대입한다.
+    // 기본형이면 변수에 들어 있는 실제 사용하는 값을 복사해서 대입하고, 참조형이면 변수에 들어 있는 참조값을 복사해서 대입한다.
     public static void main(String[] args) {
         Student student1 = createStudent("학생1", 15, 90);
         Student student2 = createStudent("학생2", 16, 80);
